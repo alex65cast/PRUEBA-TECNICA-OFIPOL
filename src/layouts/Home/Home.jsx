3b58e52fc1de281488c7eb4c604710f841a4b2d1
alex65cast <alex65578@gmail.com>
2023-06-19 13:52:21 +0200

    Feat: Navigate to pokemon with button

--- src/layouts/Home/Home.jsx
+++ src/layouts/Home/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Home.css";
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
+      navigate('/pokemon');
+  };
+  
   return (
     <>
       <div className="homeDiv">
         <img src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png" alt="Logo"></img>
         <br></br>
-        <Button variant="primary" size="lg" className="buttonHome">
+        <Button variant="primary" size="lg" className="buttonHome" onClick={handleStartClick}>
           Go Master!
         </Button>{' '}
       </div>
