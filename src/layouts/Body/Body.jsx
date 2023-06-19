af2807a4b598635ffc4503e01cf3ad1b5270ac08
alex65cast <alex65578@gmail.com>
2023-06-19 13:28:20 +0200

    Feat: Add Layouts Home, Body and CSS

---
+++ src/layouts/Body/Body.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Body.css"
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Home } from '../Home/Home';
+
+
+export const Body = () => {
+
+  return (
+    <>
+        <Routes>
+            <Route path="*" element={<Navigate to="/" />}/>
+            <Route path='/' element={<Home></Home>}/> 
+        </Routes>
+    
+    </>
+  )
+}
---
+++ src/layouts/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="homeDiv">
+        <img src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png" alt="Logo"></img>
+      </div>
+    </>
+  );
+};
