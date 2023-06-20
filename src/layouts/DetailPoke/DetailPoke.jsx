8c766b4b54af2bc8cd1357a7be9d6c4952be80c1
alex65cast <alex65578@gmail.com>
2023-06-20 11:59:11 +0200

    Feat: Detail pokemon add data from pokemon selected

--- src/layouts/DetailPoke/DetailPoke.jsx
+++ src/layouts/DetailPoke/DetailPoke.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
-import "./DetailPoke.css"
+import React, { useEffect } from "react";
+import "./DetailPoke.css";
+import { useSelector } from "react-redux";
+import { detailData } from "../detailSlice";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-export const DetailPoke=()=> {
+export const DetailPoke = () => {
+  const detailPokeRdx = useSelector(detailData);
+
+  useEffect(() => {
+    console.log(detailPokeRdx);
+  }, []);
   return (
-    <div className='desingDetail'>
-        DetailPoke
+    <div className="desingDetail">
+      <Card>
+        <Card.Header as="h5">{detailPokeRdx.name}</Card.Header>
+        <Card.Body>
+          <Card.Img variant="top" src={detailPokeRdx.sprites.front_default} />
+          
+          <Card.Title>Detail Pokemon {detailPokeRdx.name}</Card.Title>
+          <Card.Title>Abilities:</Card.Title>
+          {detailPokeRdx.abilities.map((abi) => (
+                  <Card.Text key={abi.ability.name}>
+                    -{abi.ability.name}
+                  </Card.Text>
+                ))}
+            <Card.Title> Type: </Card.Title>
+          {detailPokeRdx.types.map((typ) => (
+                  <Card.Text key={typ.type.name}>
+                   -{typ.type.name}
+                  </Card.Text>
+            ))}
+          <Card.Title>Best Moves:</Card.Title>
+          <Card.Text>-{detailPokeRdx.moves[1].move.name}</Card.Text>
+          <Card.Text>-{detailPokeRdx.moves[21].move.name}</Card.Text>
+          <Card.Text>-{detailPokeRdx.moves[90].move.name}</Card.Text>
+          <Card.Text>-{detailPokeRdx.moves[84].move.name}</Card.Text>
 
+          <Button variant="primary">Go back</Button>
+        </Card.Body>
+      </Card>
     </div>
-  )
-}
+  );
+};
--- src/layouts/Pokemon/Pokemon.jsx
+++ src/layouts/Pokemon/Pokemon.jsx
@@ -1,19 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { getPokemon, getPokemonName } from '../../services/apiCalls';
+import React, { useEffect, useState } from "react";
+import { getPokemon, getPokemonName } from "../../services/apiCalls";
 import "./Pokemon.css";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { addCharacter } from "../detailSlice";
+import { useNavigate } from "react-router-dom";
 
 export const Pokemon = () => {
   const [dataPokemon, setDataPokemon] = useState([]);
   const [bringPokemon, setBringPokemon] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const inputHandler = (e) => {
     setBringPokemon(e.target.value);
   };
 
-  useEffect(()=>{
-    console.log(bringPokemon,"haaaj")
-  })
+  useEffect(() => {
+    console.log(bringPokemon, "haaaj");
+  });
+
+  const selectedPokemon = (poke) =>{
+    console.log(poke,"Pokemon escogido");
+    dispatch(addCharacter(poke));
+    navigate("/detailPokemon")
+  }
 
   useEffect(() => {
     getPokemon()
@@ -25,53 +36,54 @@ export const Pokemon = () => {
   }, []);
 
   useEffect(() => {
-    if(bringPokemon!==""){
-    getPokemonName(bringPokemon)
-      .then((pokeData) => {
-        setDataPokemon(pokeData);
-        console.log(pokeData, "Soy pokedata2");
-      })
-      .catch((error) => console.log(error));
-    }else {
-        getPokemon()
-          .then((pokeData) => {
-            setDataPokemon(pokeData);
-            console.log(pokeData, "Soy pokedata");
-          })
-          .catch((error) => console.log(error));
-      }
-
+    if (bringPokemon !== "") {
+      getPokemonName(bringPokemon)
+        .then((pokeData) => {
+          setDataPokemon(pokeData);
+          console.log(pokeData, "Soy pokedata2");
+        })
+        .catch((error) => console.log(error));
+    } else {
+      getPokemon()
+        .then((pokeData) => {
+          setDataPokemon(pokeData);
+          console.log(pokeData, "Soy pokedata");
+        })
+        .catch((error) => console.log(error));
+    }
   }, [bringPokemon]);
 
   return (
-    <div className='pokemonDesing'>
-        <Container fluid="t" className="topCol mb-3 justify-content-center">
-          <Row>
-            <Col>
-              <input
-                className="buttonDesign"
-                type="text"
-                name="bringPokemon"
-                placeholder="Search pokemon.."
-                onChange={(e) => inputHandler(e)}
-              />
-              {/* <img
+    <div className="pokemonDesing">
+      <Container fluid="t" className="topCol mb-3 justify-content-center">
+        <Row>
+          <Col>
+            <input
+              className="buttonDesign"
+              type="text"
+              name="bringPokemon"
+              placeholder="Search pokemon.."
+              onChange={(e) => inputHandler(e)}
+            />
+            {/* <img
                 className="buscarIcon"
                 src={buscarIcon}
                 alt="buscarImagen"
               ></img> */}
-            </Col>
-          </Row>
-        </Container>
-      <div className='cardGrid'>
+          </Col>
+        </Row>
+      </Container>
+      <div className="cardGrid">
         {dataPokemon.map((pokemons) => (
-          <div className="cardColumn" key={pokemons.id}>
+          <div className="cardColumn" key={pokemons.id} onClick={()=>selectedPokemon(pokemons)}>
             <Card className="desingCardPoke">
               <Card.Img variant="top" src={pokemons.sprites.front_default} />
               <Card.Body>
                 <Card.Title>Name: {pokemons.name}</Card.Title>
                 {pokemons.types.map((typ) => (
-                  <Card.Title key={typ.type.name}>Type: {typ.type.name}</Card.Title>
+                  <Card.Title key={typ.type.name}>
+                    Type: {typ.type.name}
+                  </Card.Title>
                 ))}
               </Card.Body>
             </Card>
